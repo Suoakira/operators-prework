f44b9af2b16c4fbc9fe9c5f78e842d5ad0b72818
Stephen Courtier <steves0089@gmail.com>
2018-09-08 09:30:14 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,8 +1,11 @@
 def unsafe?(speed)
-  if  speed > 60 ?
-  elsif speed < 40 ?
-  else false
-  end
+ if speed > 60
+   return true 
+   elsif speed < 40
+   return true 
+ else
+   return false 
+ end
 end
 
 
